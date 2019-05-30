d9335b77b862b129c7465d10de5d908cb586537d
luisgc93 <luisgc93@gmail.com>
2019-05-30 19:56:38 +0100

    corrected analyse.py and reduced everything to one module

--- analyse.py
+++ analyse.py
@@ -1,10 +1,10 @@
-from nltk.corpus import PlaintextCorpusReader
 import nltk
 import os
+import csv
+from collections import OrderedDict
+from nltk.corpus import PlaintextCorpusReader
 
-# Module creates Text objects for NLP analysis
-
-# Try to do all the steps from rap.py but without all the duplicated code
+# This module creates Text objects for NLP analysis and outputs the results in a .csv file
 
 # root folder for our .txt files
 corpus_root = '/home/l21009103/Desktop/Bham_CS/NLTK/corpus/lyrics'
@@ -18,14 +18,25 @@ def analysis(directory):
         # Step 1: Create Text object
         text_obj = nltk.Text(nltk.word_tokenize(rap_corpus.raw(filename)))[0:35000]
         rapper_name = os.path.splitext(filename)[0]
-        # Step 2: Store Text objects in a dictionary with key = 'Rapper_name', value =  TextObj
-        myDict[rapper_name] = text_obj
-        #print(rapper_name)
-        for k,v in myDict.items():
-            print(k)
+        # Step 2: Check word length > 35000 & populate dictionary
+        if len(text_obj) == 35000:
+            # Key = 'Rapper_name', value =  unique word count of TextObj
+            myDict[rapper_name] = len(set(text_obj))
+    myDict = OrderedDict(sorted(myDict.items(), key=lambda t: t[1]))
+    return myDict
+
+def WriteDictToCSV(dict, csv_file):
+    with open(csv_file, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for key,value in dict.items():
+            writer.writerow([key, value])
 
 def main():
-    analysis(corpus_root)
+        dict = analysis(corpus_root)
+        print(dict)
+        currentPath = os.getcwd()
+        csv_file = currentPath + "/results.csv"
+        WriteDictToCSV(dict, csv_file)
 
 
 if __name__ == "__main__":
